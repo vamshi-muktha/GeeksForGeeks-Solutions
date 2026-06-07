e21502d891d76f2f899edf2e95fe07261933c31a
vamshi-muktha <141153452+vamshi-muktha@users.noreply.github.com>
2026-06-07 12:41:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Finding Profession/finding-profession.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String profession(int level, int pos) {
+        int flips = Integer.bitCount(pos - 1);
+
+        return (flips % 2 != 0) ? "Doctor" : "Engineer";
+    }
+}
